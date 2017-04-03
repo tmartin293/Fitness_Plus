eb75e6ab1067aa8f3b963c592b70549a0f108db5
TIEN NGUYEN <TIENQUOCNGUYEN22@GMAIL.com>
2017-04-02 17:16:57 -0700

    ProfilePage

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :user_infos
+  get 'users/show'
   root 'posts#index'
   devise_for :users, controllers: { sessions: 'users/sessions' }
   devise_for :models
   resources :posts
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
