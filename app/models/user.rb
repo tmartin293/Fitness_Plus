d0a03f1ac74b226e72fbd74d5e0e3e3984f5cd3e
TIEN NGUYEN <TIENQUOCNGUYEN22@GMAIL.com>
2017-04-04 10:35:50 -0700

    Posts in profile page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ApplicationRecord
-  
+  #has_many :post  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable 
-  has_many :posts     
   #has_one :personal_info
 end
